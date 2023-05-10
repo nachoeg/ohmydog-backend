5c7b4aa4a3874dd0d79aff9fdaaa1ae2d52fe417
Alan Staropoli <alan.edlp.st@gmail.com>
2023-05-10 19:19:51 -0300

    Cambio en la clase CORSFilter

--- src/main/java/org/acme/ohmydog/CORSFilter.java
+++ src/main/java/org/acme/ohmydog/CORSFilter.java
@@ -1,3 +1,5 @@
+package org.acme.ohmydog;
+
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
@@ -11,14 +13,8 @@ public class CORSFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         MultivaluedMap<String, Object> headers = responseContext.getHeaders();
-
-        // Permite el acceso desde cualquier origen. Ajusta esto según tus necesidades.
         headers.add("Access-Control-Allow-Origin", "*");
-
-        // Agrega los métodos HTTP permitidos
         headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-
-        // Agrega los encabezados permitidos
         headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization");
     }
 }
